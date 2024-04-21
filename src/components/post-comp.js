aeeb655e920ba4f5097085438a39d99d3f3343e4
sabhya <sabhyalokhande21@gmail.com>
2024-04-21 23:25:50 +0530

    posts

--- src/components/post-comp.js
+++ src/components/post-comp.js
@@ -1,36 +1,46 @@
-import React from 'react'
-
-import PropTypes from 'prop-types'
-
-import './post-comp.css'
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import './post-comp.css'; // Import the CSS file for PostComp
 
 const PostComp = (props) => {
+  const [text, setText] = useState('');
+
+  const handleChange = (event) => {
+    setText(event.target.value);
+  };
+
+  const handlePost = () => {
+    if (text.trim() !== '') {
+      props.onPost(text);
+      setText('');
+    }
+  };
+
   return (
-    <div className={`post-comp-container ${props.rootClassName} `}>
-      <h1 className="post-comp-text">{props.heading}</h1>
+    <div className={`post-comp-container ${props.rootClassName}`}>
+      <h2 className="post-comp-heading" style={{ marginTop: '20px', marginLeft: '28px' }}>Write a post</h2> {/* Heading with margins added */}
       <textarea
-        placeholder="Say something......"
-        className="post-comp-textarea textarea"
+        className="post-comp-textarea"
+        placeholder="Write something..."
+        value={text}
+        onChange={handleChange}
+        style={{ padding: '10px' }} // Padding added to the input box
       ></textarea>
-      <button type="button" className="post-comp-button button">
-        {props.button}
+      <button className="post-comp-button" onClick={handlePost}>
+        Post
       </button>
     </div>
-  )
-}
+  );
+};
 
 PostComp.defaultProps = {
-  heading: 'Write a post',
-  textareaPlaceholder: 'placeholder',
   rootClassName: '',
-  button: 'Post',
-}
+  onPost: () => {},
+};
 
 PostComp.propTypes = {
-  heading: PropTypes.string,
-  textareaPlaceholder: PropTypes.string,
   rootClassName: PropTypes.string,
-  button: PropTypes.string,
-}
+  onPost: PropTypes.func,
+};
 
-export default PostComp
+export default PostComp;
--- src/views/home.js
+++ src/views/home.js
@@ -1,17 +1,20 @@
-import React from 'react'
-
-import { Helmet } from 'react-helmet'
-
-import Header from '../components/header'
-import HomeCategories from '../components/home-categories'
-import CommunityPost1 from '../components/community-post-1'
-import CommunityPost2 from '../components/community-post-2'
-import CommunityPost3 from '../components/community-post-3'
-import PostComp from '../components/post-comp'
-import Footer1 from '../components/footer1'
-import './home.css'
+import React, { useState } from 'react';
+import { Helmet } from 'react-helmet';
+import Header from '../components/header';
+import HomeCategories from '../components/home-categories';
+import CommunityPost1 from '../components/community-post-1';
+import PostComp from '../components/post-comp';
+import Footer1 from '../components/footer1';
+import './home.css';
 
 const Home = (props) => {
+  const [posts, setPosts] = useState([]);
+
+  const handlePost = (text) => {
+    // Add the new post to the array of posts
+    setPosts([...posts, text]);
+  };
+
   return (
     <div className="home-container">
       <Helmet>
@@ -24,15 +27,25 @@ const Home = (props) => {
       <div className="home-container2">
         <HomeCategories rootClassName="home-categories-root-class-name"></HomeCategories>
         <div className="home-container3">
-          <CommunityPost1 rootClassName="community-post1-root-class-name"></CommunityPost1>
-          <CommunityPost2 rootClassName="community-post2-root-class-name"></CommunityPost2>
-          <CommunityPost3 rootClassName="community-post3-root-class-name"></CommunityPost3>
+          {/* Render each post from the posts array */}
+          {posts.map((post, index) => (
+            <CommunityPost1
+              key={index}
+              heading="New Post"
+              text1={post} // Pass the post text as text1 prop
+              rootClassName="community-post1-root-class-name"
+            />
+          ))}
         </div>
-        <PostComp rootClassName="post-comp-root-class-name"></PostComp>
+        {/* Pass the handlePost function to the PostComp component */}
+        <PostComp
+          rootClassName="post-comp-root-class-name"
+          onPost={handlePost}
+        />
       </div>
       <Footer1 rootClassName="footer1-root-class-name"></Footer1>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
